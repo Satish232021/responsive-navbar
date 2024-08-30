c726d2afe6289f5139956d6652455953d951996c
Satish232021 <100710941+Satish232021@users.noreply.github.com>
2024-08-30 20:25:31 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-top-up").addEventListener("click", function(){
+    document.querySelector(".sidebar").style.display = "flex";
+});
+
+document.querySelector(".close-menu").addEventListener("click",function(){
+    document.querySelector(".sidebar").style.display="none";
+});
